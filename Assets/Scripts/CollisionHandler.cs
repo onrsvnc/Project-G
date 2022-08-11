6042c5afd3578e2bfc27fd05e39092d4579a2ea7
onrsvnc <onrsvnc@hotmail.com>
2022-08-11 15:03:54 +0300

    VFX & Instantiate
    -VFX for Player and Enemy created.
    -Instantiation of VFX codded.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,31 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    void OnCollisionEnter(Collision other) 
-    {
-        Debug.Log(this.name + " Collided With " + other.gameObject.name);
-        // switch (other.gameObject.tag)   //Switch method for debugging
-        // {
-        //     case "Enemy":
-        //     Debug.Log("Collided with Enemy");
-        //     break;
+    [SerializeField] float reloadLevelDelay = 1f;
+    [SerializeField] ParticleSystem deathExplosion;
+    [SerializeField] ParticleSystem laserR;
+    [SerializeField] ParticleSystem laserL;
 
-        //     case "Pipe":
-        //     Debug.Log("Collided with Pipe");
-        //     break;
+
+    // void OnCollisionEnter(Collision other) 
+    // {
+    //     switch (other.gameObject.tag)   //Switch method for debugging
+    //     {
+    //         case "Enemy":
+    //         Debug.Log("Collided with Enemy");
+    //         break;
+
+    //         case "Pipe":
+    //         Debug.Log("Collided with Pipe");
+    //         break;
             
-        //     case "Environment":
-        //     Debug.Log("Collided with Environment");
-        //     break;
-        // }
-    } 
-    void OnTriggerEnter(Collider other) 
+    //         case "Environment":
+    //         Debug.Log("Collided with Environment");
+    //         break;
+    //     }
+    // } 
+    void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Triggered!! by " + other.gameObject.name);
-    } 
+        // Debug.Log("Triggered!! by " + other.gameObject.name);
+        StartCrashSequence();
+    }
+
+    void StartCrashSequence()
+    {
+        deathExplosion.Play();
+        DisableLasers();
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<MeshCollider>().enabled = false;
+        GetComponentInParent<PlayerController>().enabled = false;
+        Invoke("ReloadLevel", reloadLevelDelay);
+    }
+
+    void DisableLasers()
+    {
+        var emmisionR = laserR.emission;
+        var emmisionL = laserL.emission;
+        emmisionR.enabled = false;
+        emmisionL.enabled = false;
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+    
 
     
 }
