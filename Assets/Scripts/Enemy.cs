a0e9c56984b86c5450847b28ae6647bb5c86e219
onrsvnc <onrsvnc@hotmail.com>
2022-08-17 14:08:44 +0300

    UI, Score, Voice Lines
    -UI added with live scoreboard.
    -Dialogue added with UI pop-ups.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,53 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
-    [SerializeField] Transform parent;
+    [SerializeField] GameObject hitVfx;
+    [SerializeField] int scoreGained;
+    [SerializeField] int health;
+    GameObject SpawnAtRuntime;
+    ScoreBoard scoreBoard;
+    
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+        SpawnAtRuntime = GameObject.FindWithTag("Spawn at Runtime");
+       // gameObject.AddComponent<Rigidbody>().useGravity = false; 
+       // with "add component" we add rigid body for some enemy models that only has colliders in their children so our shots would work but its not needed here
+    }
     void OnParticleCollision(GameObject other)
     {
-        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
-        Destroy(gameObject, 1f);
+        ProcessHitVFX();
+        ProcessHealth();
     }
+
+    void ProcessHitVFX()
+    {
+        GameObject hitVfxIns = Instantiate(hitVfx, transform.position, Quaternion.identity);
+        hitVfxIns.transform.parent = SpawnAtRuntime.transform;
+    }
+    void ProcessScore()
+    {
+        scoreBoard.ModifyScore(scoreGained);
+    }
+
+    void ProcessHealth()
+    {
+        if (health >= Mathf.Epsilon)
+        {
+            health --; // Consider that we have dual lasers so we will be doing double damage
+        }
+        else if (health <= Mathf.Epsilon)
+        {
+            ProcessDeath();
+        }
+    }
+
+    void ProcessDeath()
+    {
+        ProcessScore();
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = SpawnAtRuntime.transform;
+        Destroy(gameObject);
+    }
+
 }
